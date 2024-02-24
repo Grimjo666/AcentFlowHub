19dd3c7f5ed6bb90cf361802d3b1ccbd097ef4ac
Grimjo666 <uinstonmartis@gmail.com>
2024-02-24 20:15:33 +0300

    added a basic model and html for progress circles

--- AscentFlowHub_project/AscentFlowHub_API/models.py
+++ AscentFlowHub_project/AscentFlowHub_API/models.py
@@ -1,2 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+from transliterate import translit
+
+
+class ProgressCircleModel(models.Model):
+    name = models.CharField(max_length=25, null=False)
+    latin_name = models.CharField(max_length=25, default='', blank=True)
+    percent = models.IntegerField(default=0)
+    first_color = models.CharField(max_length=20, blank=True)
+    second_color = models.CharField(max_length=20, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @staticmethod
+    def custom_translit(text):
+        try:
+            # Если текст является кириллическим
+            result = translit(text, 'ru', reversed=True)
+        except Exception:
+            # Если текст не является кириллическим
+            result = text
+        return result
+
+    def save(self, *args, **kwargs):
+        self.latin_name = self.custom_translit(self.name)
+        super(LifeSphereModel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name} {self.percent}'
--- AscentFlowHub_project/AscentFlowHub_web/admin.py
+++ AscentFlowHub_project/AscentFlowHub_web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from AscentFlowHub_API import models
+
+admin.site.register(models.ProgressCircleModel)
